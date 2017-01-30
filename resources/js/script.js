2c767f3d08df80327537c3f8e56bc0a6e1538036
linocatucci <Lino@MacBook-Pro-Peachland.local>
2017-01-30 22:24:42 +0100

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,11 @@
+var name = 'John';
+var age = 26;
+var job = 'teacher';
+
+//console.log(name + ' ' + age + ' ' + job);
+
+var tweedenaam = prompt('wat is je naam?');
+
+console.log(tweedenaam);
+
+alert(tweedenaam);
