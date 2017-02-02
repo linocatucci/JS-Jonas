b84373aa879be02866cac9a8a4a95540838f1aba
linocatucci <Lino@MacBook-Pro-Peachland.local>
2017-02-02 06:33:07 +0100

    loops, while and for,  continue and break

--- resources/js/script.js
+++ resources/js/script.js
@@ -355,3 +355,54 @@ console.log(OpelAstra.engine, OpelAstra.horse_power);
 
 
 //lecture: loops
+// simple for loop
+/*
+for (var i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+var names = ['Josh', 'Mike', 'Suzan', 'Julia', 'Bob'];
+console.log(names);
+
+//print the 5 names in the console
+names[0];
+names[1]; //bla bla
+/*
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+*/
+/*
+// 1 >= 0 when false the iteration/loop will go on!
+for (var i = names.length - 1; i >= 0; i--) {
+    console.log(names[i]);
+}
+*/
+
+// while loop
+// condition is evaluated
+var names = ['Josh', 'Mike', 'Suzan', 'Julia', 'Bob'];
+console.log(names);
+var i = 0;
+while (i < names.length) {
+    console.log(names[i]);
+    i++;
+}
+
+// break is break out of the loop
+// continue is skip an iteration / value
+
+for (i = 1; i <= 5; i++) {
+    console.log(i);
+
+    if (i === 3) {
+        break;
+    }
+}
+
+for (i = 1; i <= 5; i++) {
+    if (i === 3) {
+        continue;
+    }
+    console.log(i);
+}
