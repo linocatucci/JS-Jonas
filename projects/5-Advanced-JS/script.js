30668b892ce9a65ff977414d96234bb9f5f75fa9
linocatucci <Lino@MacBook-Pro-Peachland.local>
2017-02-09 20:38:46 +0100

    advanced javascript

--- projects/5-Advanced-JS/script.js
+++ projects/5-Advanced-JS/script.js
@@ -77,6 +77,8 @@ console.log(Opel_Astra.calculateYearlyCost());
 // function construction is used more, so this type we
 // will use in the course.
 
+/*
+
 var personProto = {
     calculateAge: function () {
         console.log(2016 - yearOfBrith);
@@ -103,3 +105,193 @@ var jane = Object.create(personProto, {
     }
 
 });
+
+*/
+
+// lecture: functions: passing functions as arguments and use of callback functions
+
+var years = [1990, 2005, 1971, 2006, 1975, 1965];
+
+// generic function which loops over an arry and an input of a function
+function arrayCalc(arr, fn) {
+    var arrResult = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        // 1990 wordt in de functie calculateAge gedaan
+        arrResult.push(fn(arr[i]));
+    }
+    return arrResult;
+}
+
+// callback functions (el input zal de array years zijn)
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function iSfullAge(el) {
+    return el >= 18;
+}
+
+// el is the age
+function maxHearthRate(el) {
+
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+
+    } else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, iSfullAge);
+var rates = arrayCalc(ages, maxHearthRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
+
+// lecture: functions returning functions
+// for each job it's returning a function which returns a name and a string
+
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+            console.log(name + ' can you please explain what UX design is? ');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log('what subject do you teach? ' + name);
+        }
+    } else {
+        return function (name) {
+            console.log('hello what do you do? ');
+        }
+    }
+};
+// returning a function from a function in var teacherQuestion.
+var teacherQuestion = interviewQuestion('teacher');
+
+teacherQuestion('John');
+
+// kan ook zo aangeroepen worden
+interviewQuestion('teacher')('John')
+
+var designerQuestion = interviewQuestion('designer');
+
+designerQuestion('Julia');
+designerQuestion('Mike');
+designerQuestion('Jane');
+designerQuestion('Floor');
+
+
+// welke school? wit, zwart of gemengd?
+
+function welkeSchool(kleur) {
+    if (kleur === 'wit') {
+        return function (naam) {
+            console.log(naam + ' Jij kunt beter naar Saenredam school gaan');
+        }
+    } else if (kleur === 'gemengd') {
+        return function (naam) {
+            console.log(naam + ' Jij kunt beter naar Compaan gaan');
+        }
+    } else {
+        return function (naam) {
+            console.log(naam + ' Jij past beter bij Poelenburg');
+        }
+    }
+};
+
+var witteVraag = welkeSchool('wit');
+witteVraag('Julia');
+// eigenlijk worden het 2 input parameters voor de functie:
+// welkeSchool ('wit')('Julia');
+
+var gemengdeVraag = welkeSchool('gemengd');
+gemengdeVraag('Floor');
+
+var zwarteVraag = welkeSchool();
+zwarteVraag('Brian');
+
+
+// immediately invoked function expressions (IIFE)
+
+
+function game() {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+    console.log(score);
+
+}
+game();
+
+// kan ook zo geschreven worden. Als een functie alleen maar een lokale variabele moet hiding for the outside, kun je beter IIFE gebruiken.
+// trick the javascript parser door in () te "wrappen"
+// dan denkt javascript dat het een expression is en niet een declaration of the function.
+// data privacy here!
+(function () {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
+
+
+(function (goodluck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodluck);
+    console.log(score);
+})(5);
+
+
+
+function retirement(retirementAge) {
+    var a = ' years left until retirement';
+    return function (yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    };
+}
+// maak een var met functie van het eerste deel
+// daarna die variabele aanroepen met de input parametern van de 2e functie (yearOfBirth)
+var retirementUS = retirement(66);
+retirementUS(1990);
+// kan ook zo aangeroepen worden
+retirement(66)(1990);
+
+var retirementNL = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementNL(1971);
+retirementIceland(1972);
+
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+            console.log(name + ' can you please explain what UX design is? ');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log('what subject do you teach? ' + name);
+        }
+    } else {
+        return function (name) {
+            console.log('hello what do you do? ');
+        }
+    }
+};
+*/
+
+function interviewQuestion(job) {
+    return function (name) {
+        if (job === 'teacher') {
+            console.log(name + ' You should be teacher');
+        } else if (job === 'designer') {
+
+            console.log(name + ' You should be designer');
+        } else {
+            //code
+        }
+    };
+}
+interviewQuestion('teacher')('John');
