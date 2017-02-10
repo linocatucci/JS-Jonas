46e28452b67909ccceb9d024987e4736a598e7c4
linocatucci <Lino@MacBook-Pro-Peachland.local>
2017-02-10 17:28:47 +0100

    coding challange

--- projects/4-Coding challange 4/script.js
+++ projects/4-Coding challange 4/script.js
@@ -8,7 +8,7 @@ c. correct answer (i would use a number for this)
 
 2. create a couple of questions using the constructor
 
-3.store them all inside an array
+3. store them all inside an array
 
 4. select one random question and log it on the console, together with the possible answers (each question should have a number (hint: write a method for the question object for this task)).
 
@@ -26,3 +26,23 @@ a. question itself
 b. the answer from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
 c. correct answer (i would use a number for this)
 */
+
+/*
+braindump, vraag, antwoord is ja en nee, ja = 0 nee is 1
+
+*/
+// create the function constructor "question"
+function Question(question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+};
+
+// create 3 questions
+var q1 = new Question('Is JavaScript the coolest programming language in the world? ', ['Yes', 'No'], 0);
+var q2 = new Question('What is the name of the courses\'s teacher? ', ['Brian', 'Michael', 'Jonas'], 3);
+var q3 = new Question('What is the best IDE for Javascript', ['Brackets', 'Atom', 'VSCode'], 3);
+
+// store the questions in an array
+
+var questions = [q1, q2, q3];
