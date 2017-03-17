1d92fcd15b583ab4b721d7dbd54f3fd0964c7a59
linocatucci <Lino@Fernie.local>
2017-03-17 11:40:38 +0100

    last change

--- projects/5-Advanced-JS/5-Advanced-JS-Final/script.js
+++ projects/5-Advanced-JS/5-Advanced-JS-Final/script.js
@@ -1,19 +1,19 @@
 /////////////////////////////
 // Lecture: Function constructor
-/*
+
 var john = {
     name: 'John',
     yearOfBirth: 1990,
     job: 'teacher'
 };
 
-var Person = function(name, yearOfBirth, job) {
+var Person = function (name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
 }
 
-Person.prototype.calculateAge  = function() {
+Person.prototype.calculateAge = function () {
     console.log(2016 - this.yearOfBirth);
 };
 
